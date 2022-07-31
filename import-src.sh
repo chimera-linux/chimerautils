67cf5d0c12d4be78978d44fe6e32743828160f09
Daniel Kolesa <daniel@octaforge.org>
2022-07-31 20:50:00 +0200

    build: split up the source dir a little

--- import-src.sh
+++ import-src.sh
@@ -26,110 +26,16 @@ cd ${TMPDIR}
 curl -L --retry 3 --ftp-pasv -O ${SRC} || fail_exit
 xz -dc src.txz | tar -xf -
 
-# copy in the source for all the programs we want
-CMDS="bin/test
-      usr.bin/basename
-      usr.bin/bc
-      bin/cat
-      bin/chmod
-      usr.sbin/chown
-      usr.bin/cksum
-      usr.bin/cmp
-      usr.bin/col
-      usr.bin/colrm
-      usr.bin/column
-      usr.bin/comm
-      bin/cp
-      usr.bin/csplit
-      usr.bin/cut
-      bin/date
-      usr.bin/dc
-      bin/dd
-      bin/df
-      usr.bin/diff
-      usr.bin/dirname
-      usr.bin/du
-      bin/echo
-      bin/ed
-      usr.bin/env
-      usr.bin/expand
-      bin/expr
-      usr.bin/factor
-      usr.bin/false
-      usr.bin/find
-      usr.bin/fmt
-      usr.bin/fold
-      usr.bin/getopt
-      usr.bin/grep
-      usr.bin/gzip
-      usr.bin/head
-      usr.bin/hexdump
-      bin/hostname
-      usr.bin/id
-      usr.bin/join
-      bin/kill
-      bin/ln
-      usr.bin/logname
-      usr.bin/look
-      bin/ls
-      usr.bin/m4
-      usr.bin/mesg
-      bin/mkdir
-      sbin/mknod
-      usr.bin/mktemp
-      usr.bin/mkfifo
-      bin/mv
-      usr.bin/nice
-      usr.bin/nl
-      usr.bin/nohup
-      usr.bin/paste
-      usr.bin/patch
-      usr.bin/pr
-      usr.bin/printenv
-      usr.bin/printf
-      bin/pwd
-      bin/realpath
-      usr.bin/renice
-      usr.bin/rev
-      bin/rm
-      bin/rmdir
-      usr.bin/sdiff
-      usr.bin/sed
-      usr.bin/seq
-      bin/sh
-      bin/sleep
-      usr.bin/sort
-      usr.bin/split
-      usr.bin/stat
-      usr.bin/stdbuf
-      bin/stty
-      bin/sync
-      usr.bin/tail
-      usr.bin/tee
-      usr.bin/timeout
-      usr.bin/touch
-      usr.bin/tr
-      usr.bin/true
-      usr.bin/truncate
-      usr.bin/tsort
-      usr.bin/tty
-      usr.bin/ul
-      usr.bin/uname
-      usr.bin/unexpand
-      usr.bin/uniq
-      usr.bin/users
-      usr.bin/wall
-      usr.bin/wc
-      usr.bin/which
-      usr.bin/who
-      usr.bin/write
-      usr.bin/yes
-      usr.sbin/chroot
-      usr.bin/xargs
-      usr.bin/xinstall"
-for p in ${CMDS} ; do
+copy_cmd() {
+    p="$1"
+    sd="$2"
     rp="usr/src/${p}"
     sp="$(basename ${p})"
+    if [ -n "$sd" ]; then
+        dp="${sd}/${sp}"
+    else
+        dp="$sp"
+    fi
 
     # Drop the tests/ subdirectories
     [ -d ${rp}/tests ] && rm -rf ${rp}/tests
@@ -145,9 +51,152 @@ for p in ${CMDS} ; do
     rm -f ${rp}/Makefile.depend*
 
     # Copy in the upstream files
-    [ -d ${CWD}/src/${sp} ] || mkdir -p ${CWD}/src/${sp}
-    cp -pr ${rp}/* ${CWD}/src/${sp}
+    [ -d ${CWD}/src/${dp} ] || mkdir -p ${CWD}/src/${dp}
+    cp -pr ${rp}/* ${CWD}/src/${dp}
+}
+
+# coreutils
+CMDS_CORE="
+bin/cat
+bin/chmod
+bin/cp
+bin/date
+bin/dd
+bin/df
+bin/echo
+bin/expr
+bin/hostname
+bin/ln
+bin/ls
+bin/mkdir
+bin/mv
+bin/pwd
+bin/realpath
+bin/rm
+bin/rmdir
+bin/sleep
+bin/stty
+bin/sync
+bin/test
+sbin/mknod
+usr.bin/basename
+usr.bin/cksum
+usr.bin/comm
+usr.bin/cut
+usr.bin/csplit
+usr.bin/dirname
+usr.bin/du
+usr.bin/env
+usr.bin/expand
+usr.bin/factor
+usr.bin/false
+usr.bin/fmt
+usr.bin/fold
+usr.bin/head
+usr.bin/id
+usr.bin/join
+usr.bin/logname
+usr.bin/mktemp
+usr.bin/mkfifo
+usr.bin/nice
+usr.bin/nl
+usr.bin/nohup
+usr.bin/paste
+usr.bin/pathchk
+usr.bin/pr
+usr.bin/printenv
+usr.bin/printf
+usr.bin/seq
+usr.bin/sort
+usr.bin/split
+usr.bin/stat
+usr.bin/stdbuf
+usr.bin/tail
+usr.bin/tee
+usr.bin/timeout
+usr.bin/touch
+usr.bin/tr
+usr.bin/true
+usr.bin/truncate
+usr.bin/tsort
+usr.bin/tty
+usr.bin/uname
+usr.bin/unexpand
+usr.bin/uniq
+usr.bin/users
+usr.bin/wc
+usr.bin/who
+usr.bin/yes
+usr.bin/xinstall
+usr.sbin/chown
+usr.sbin/chroot
+"
+
+# diffutils
+CMDS_DIFF="
+usr.bin/cmp
+usr.bin/diff
+usr.bin/sdiff
+"
+
+# findutils
+CMDS_FIND="
+usr.bin/find
+usr.bin/xargs
+"
+
+# bc
+CMDS_BC="
+usr.bin/bc
+usr.bin/dc
+"
+
+# mostly util-linux
+CMDS_MISC="
+bin/kill
+usr.bin/col
+usr.bin/colrm
+usr.bin/column
+usr.bin/getopt
+usr.bin/hexdump
+usr.bin/look
+usr.bin/mesg
+usr.bin/renice
+usr.bin/rev
+usr.bin/ul
+usr.bin/wall
+usr.bin/write
+"
+
+for p in ${CMDS_CORE}; do
+    copy_cmd "$p" coreutils
 done
+
+for p in ${CMDS_DIFF}; do
+    copy_cmd "$p" diffutils
+done
+
+for p in ${CMDS_FIND}; do
+    copy_cmd "$p" findutils
+done
+
+for p in ${CMDS_BC}; do
+    copy_cmd "$p" bc
+done
+
+for p in ${CMDS_MISC}; do
+    copy_cmd "$p" miscutils
+done
+
+# equivalents of standalone projects
+copy_cmd bin/ed
+copy_cmd bin/sh
+copy_cmd usr.bin/grep
+copy_cmd usr.bin/gzip
+copy_cmd usr.bin/m4
+copy_cmd usr.bin/patch
+copy_cmd usr.bin/sed
+copy_cmd usr.bin/which
 
 # 'compat' is our static library with a subset of BSD library functions
 cp -p usr/src/lib/libc/gen/setmode.c ${CWD}/compat
@@ -163,17 +212,17 @@ cp -p usr/src/contrib/libc-vis/vis.h ${CWD}/include
 cp -p usr/src/lib/libopenbsd/ohash.h ${CWD}/include
 
 # These files are needed for the factor command
-cp -p usr/src/usr.bin/primes/primes.h ${CWD}/src/factor
-cp -p usr/src/usr.bin/primes/pr_tbl.c ${CWD}/src/factor
+cp -p usr/src/usr.bin/primes/primes.h ${CWD}/src/coreutils/factor
+cp -p usr/src/usr.bin/primes/pr_tbl.c ${CWD}/src/coreutils/factor
 
 # These files are needed for the df command
-cp -p usr/src/sbin/mount/vfslist.c ${CWD}/src/df
+cp -p usr/src/sbin/mount/vfslist.c ${CWD}/src/coreutils/df
 
 # These are not used
-rm -rf ${CWD}/src/sort/nls
+rm -rf ${CWD}/src/coreutils/sort/nls
 
 # sort manpage
-mv ${CWD}/src/sort/sort.1.in ${CWD}/src/sort/sort.1
+mv ${CWD}/src/coreutils/sort/sort.1.in ${CWD}/src/coreutils/sort/sort.1
 
 # fix sh generator permissions
 chmod 755 ${CWD}/src/sh/mkbuiltins
@@ -198,13 +247,12 @@ fi
 
 if [ -d ${CWD}/patches/src ]; then
     cd ${CWD}/patches/src
-    for subdir in * ; do
-        [ -d ${subdir} ] || continue
-        for patchfile in ${CWD}/patches/src/${subdir}/*.patch ; do
-            destfile="$(basename ${patchfile} .patch)"
-            [ -f "${CWD}/src/${subdir}/${destfile}.orig" ] && rm -f "${CWD}/src/${subdir}/${destfile}.orig"
-            patch -d ${CWD}/src -p0 -b -z .orig < ${patchfile}
-        done
+    for patchfile in $(find . -name '*.patch') ; do
+        [ -f "${patchfile}" ] || continue
+        destfile="$(basename ${patchfile} .patch)"
+        subdir="$(dirname ${patchfile})"
+        [ -f "${CWD}/src/${subdir}/${destfile}.orig" ] && rm -f "${CWD}/src/${subdir}/${destfile}.orig"
+        patch -d ${CWD}/src/${subdir} -p1 -b -z .orig < ${patchfile}
     done
 fi
 
