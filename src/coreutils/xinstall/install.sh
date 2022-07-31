443fc96d4dd1ecccf8abafd8282de9db44523ed0
Daniel Kolesa <daniel@octaforge.org>
2022-08-01 01:26:09 +0200

    install(1), test(1): fix install.sh scripts

--- src/coreutils/xinstall/install.sh
+++ src/coreutils/xinstall/install.sh
@@ -4,4 +4,4 @@ BINDIR="${1}"
 
 mkdir -p "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"
 install -d "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"
-install -m 0755 ${MESON_BUILD_ROOT}/src/xinstall/xinstall "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"/install
+install -m 0755 ${MESON_BUILD_ROOT}/src/coreutils/xinstall/xinstall "${DESTDIR}/${MESON_INSTALL_PREFIX}/${BINDIR}"/install
