020c754a47e2a7221acb7bafc3ce6322024bd534
Daniel Kolesa <daniel@octaforge.org>
2022-08-05 23:20:25 +0200

    df(1): fix network filesystem list

--- src/coreutils/df/df.c
+++ src/coreutils/df/df.c
@@ -77,7 +77,7 @@ __FBSDID("$FreeBSD$");
  * list should be made in to something we can generate at runtime or
  * just expand the list.
  */
-#define NETVFSLIST "nonfs,nosmb,nocifs"
+#define NETVFSLIST "nonfs,smb,cifs"
 
 /* combining data from getmntent() and statvfs() on Linux */
 struct mntinfo {
