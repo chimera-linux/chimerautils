2bd5175e0ee265109f862f6ef26f2a1bb1e1be9b
q66 <q66@chimera-linux.org>
2024-09-29 17:31:32 +0200

    also do the same for rpmatch.h

--- src.freebsd/include/rpmatch.h
+++ src.freebsd/include/rpmatch.h
@@ -7,7 +7,15 @@
 
 #if !defined(_CHIMERAUTILS_BUILD) || !defined(HAVE_RPMATCH)
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern int rpmatch(const char *response);
+
+#ifdef __cplusplus
+}
+#endif
 
 #else
 #  include <stdlib.h>
