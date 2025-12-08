cb9b9c455375a7d2acf4393f48e31f1f5ea4b43f
q66 <q66@chimera-linux.org>
2025-12-08 12:30:39 +0100

    ls(1): only zero out fts_statp if FTS_NOSTAT is not set
    The structure is not set/allocated with that option.

--- src.freebsd/coreutils/ls/ls.c
+++ src.freebsd/coreutils/ls/ls.c
@@ -728,7 +728,7 @@ traverse(int argc, char *argv[], int options)
  * points to the parent directory of the display list.
  */
 static void
-display(const FTSENT *p, FTSENT *list, int options __unused)
+display(const FTSENT *p, FTSENT *list, int options)
 {
 	struct stat *sp;
 	DISPLAY d;
@@ -818,7 +818,8 @@ display(const FTSENT *p, FTSENT *list, int options __unused)
 			}
 			cur->fts_number = ERR_PRINT;
 			/* zero out the structure just in case */
-			memset(cur->fts_statp, 0, sizeof(*cur->fts_statp));
+			if (!(options & FTS_NOSTAT))
+				memset(cur->fts_statp, 0, sizeof(*cur->fts_statp));
 			failent = true;
 		} else failent = false;
 		/*
